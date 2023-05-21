08030212b016267257b433113c26ed72a58d1b91
Crowdbotics <team@crowdbotics.com>
2023-05-21 00:03:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snowy-rice-40869",
+  url: "https://snowy-rice-40869.botics.co",
+  api: "https://snowy-rice-40869.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
